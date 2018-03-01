bbef0eaa2776dbccb2b52f5662e0a1391c4bc7d0
RyoNkmr <ryonakamuraryo@gmail.com>
2018-03-01 16:27:08 +0900

    first commit lifegame

---
+++ components/LifeCell.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    class="view"
+    :style="{ backgroundColor: hsl }"
+  >{{ message }}</div>
+</template>
+<script>
+export default {
+  props: {
+    state: {
+      type: Number,
+      default: 0,
+    },
+    live: {
+      type: String,
+      default: '😂',
+    },
+    dead: {
+      type: String,
+      default: '💀',
+    },
+    hueRatio: {
+      type: Number,
+      default: 7,
+    },
+    satuation: {
+      type: Number,
+      default: 100,
+    },
+    luminance: {
+      type: Number,
+      default: 80,
+    },
+  },
+  computed: {
+    message() {
+      return this.state === 0 ? this.dead : this.live;
+    },
+    hsl() {
+      return this.state === 0
+        ? `hsl(0, 0%, 20%)`
+        : `hsl(${this.state * this.hueRatio}, ${this.satuation}%, ${this.luminance}%)`;
+    },
+  },
+};
+</script>
+<style>
+.view {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  width: 2rem;
+  height: 2rem;
+}
+</style>
---
+++ components/LifeGame.vue
@@ -0,0 +1,153 @@
+<template>
+  <div>
+    <div class="LifeGame">
+      <div class="LifeGameView">
+        <div
+          class="LifeGameRow"
+          v-for="(row, rowIndex) in cells"
+          :key="rowIndex"
+        >
+          <life-cell
+            :state="cell"
+            @mousedown.native.prevent="paintStart(rowIndex, columnIndex)"
+            @mouseenter.native="paint === null ? null : paintCell(rowIndex, columnIndex, $event)"
+            v-for="(cell, columnIndex) in row"
+            :key="`${rowIndex}x${columnIndex}`"
+          />
+        </div>
+      </div>
+    </div>
+    <p>{{ frame }}</p>
+    <input v-model="newSize" type="number" @keyup.enter="setNewSize" />
+    <button @click.prevent="setNewSize">
+      {{
+        size === newSize
+        ? `reset`
+        : `set new size ${newSize}`
+      }}</button>
+    <hr />
+    <button @click.prevent="goNext">Next</button>
+    <hr />
+    <input v-model="interval" type="number" step="10" @keyup.enter="auto = !auto" /> ms
+    <button @click.prevent="auto = !auto">
+      {{ auto ? 'stop' : 'start' }}
+    </button>
+  </div>
+</template>
+<script>
+import LifeCell from '~/components/LifeCell.vue';
+
+const N = [-1, 0, 1];
+const MINIMUM_INTERVAL_MS = 150;
+export default {
+  components: {
+    LifeCell,
+  },
+  data() {
+    return {
+      auto: false,
+      interval: 600,
+      timerId: null,
+      size: 3,
+      newSize: 10,
+      cells: [[0,0,0], [0,0,0], [0,0,0]],
+      frame: 0,
+      paint: null,
+      painted: new Set(),
+    };
+  },
+  watch: {
+    size(length) {
+      this.auto = false;
+    },
+    auto(value) {
+      value ? this.startAuto() : this.stopAuto();
+    }
+  },
+  methods: {
+    paintStart(rowIndex, columnIndex) {
+      this.updateCellState(rowIndex, columnIndex, Math.abs(this.cells[rowIndex][columnIndex] - 1));
+      this.paint = this.cells[rowIndex][columnIndex];
+    },
+    updateCellState(rowIndex, columnIndex, state) {
+      const newRow = this.cells[rowIndex].map((current, index) => (
+        index === columnIndex ? state : current
+      ));
+      this.cells.splice(rowIndex, 1, newRow);
+    },
+    paintEnd() {
+      this.painted.clear();
+      this.paint = null;
+    },
+    paintCell(rowIndex, columnIndex, event) {
+      if (event.buttons === 0 && this.painted.size > 0) {
+        this.paintEnd();
+        return;
+      }
+      if (this.painted.has(`${rowIndex}${columnIndex}`)) {
+        return;
+      }
+      this.updateCellState(rowIndex, columnIndex, this.paint);
+      this.painted.add(`${rowIndex}${columnIndex}`);
+    },
+    startAuto() {
+      this._loop();
+    },
+    stopAuto() {
+      if (this.timerId !== null) {
+        clearTimeout(this.timerId);
+        this.timerId = null;
+      }
+    },
+    _loop() {
+      this.goNext();
+      this.timerId = setTimeout(this._loop, Math.min(this.interval, MINIMUM_INTERVAL_MS));
+    },
+    setNewSize() {
+      this.size = this.newSize;
+      this.auto = false;
+      this.frame = 0;
+      this.cells = Array.from({ length: this.size }).map(_ => Array.from({ length: this.size }).fill(0));
+    },
+    goNext() {
+      this.cells = this.cells.map((_row, rowIndex) => (
+        _row.map((state, columnIndex) => this._lifecycle(rowIndex, columnIndex, state))
+      ));
+      this.frame += 1;
+    },
+    _lifecycle(rowIndex, columnIndex, state) {
+      const points = N.reduce((sum, relativeRowIndex) => (
+        sum + N.reduce((rowSum, relativeColumnIndex) => (
+          rowSum + (
+            this.cells[rowIndex + relativeRowIndex]
+            ? (this.cells[rowIndex + relativeRowIndex][columnIndex + relativeColumnIndex] > 0 ? 1 : 0)
+            : 0
+          )
+        ), 0)
+      ), 0);
+      return this.generateNewState(points, state);
+    },
+    generateNewState(points, state) {
+      const pointsAround = points - Math.min(state, 1);
+      return state === 0
+        ? (pointsAround === 3 ? 1 : 0)
+        : (pointsAround === 2 || pointsAround === 3 ? state + 1 : 0);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.LifeGame {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &View {
+    display: flex;
+    flex-direction: column;
+  }
+  &Row {
+    display: flex;
+  }
+}
+</style>
